557bf8be658ce4ba36856629aed57f080d918112
Philip Helger <philip@helger.com>
2018-02-13 14:56:34 +0100

    Fixed compile error

--- src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
+++ src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
@@ -1,33 +1,23 @@
 package eu.toop.demoui.endpoints;
 
-import eu.toop.iface.ITOOPInterfaceDC;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import eu.toop.iface.ITOOPInterfaceDC;
 
 public class TOOPInterfaceDC implements ITOOPInterfaceDC {
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDC (doGet)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
-    }
-
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDC (doPost)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
+    public void doPost(final HttpServletRequest req, final HttpServletResponse resp)
+	    throws ServletException, IOException {
+	final String pathInfo = req.getPathInfo();
+	final PrintWriter aPW = resp.getWriter();
+	aPW.write("<html><body><h1>TOOPInterfaceDC (doPost)</h1>");
+	aPW.write("<div>Path info: " + pathInfo + "</div>");
+	aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
+	aPW.write("</body></html>");
+	aPW.flush();
     }
 }
--- src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDP.java
+++ src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDP.java
@@ -1,33 +1,24 @@
 package eu.toop.demoui.endpoints;
 
-import eu.toop.iface.ITOOPInterfaceDP;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import eu.toop.iface.ITOOPInterfaceDP;
 
 public class TOOPInterfaceDP implements ITOOPInterfaceDP {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDP (doGet)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
-    }
-
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDP (doPost)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
+    public void doPost(final HttpServletRequest req, final HttpServletResponse resp)
+	    throws ServletException, IOException {
+	final String pathInfo = req.getPathInfo();
+	final PrintWriter aPW = resp.getWriter();
+	aPW.write("<html><body><h1>TOOPInterfaceDP (doPost)</h1>");
+	aPW.write("<div>Path info: " + pathInfo + "</div>");
+	aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
+	aPW.write("</body></html>");
+	aPW.flush();
     }
 }
