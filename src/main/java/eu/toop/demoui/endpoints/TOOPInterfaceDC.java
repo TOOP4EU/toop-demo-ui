20869eb26c9ec3ca30166d8ed6231fbed18455ad
Anton Wiklund <ixuz07@gmail.com>
2018-02-13 17:08:45 +0100

    Removed the GET handling

--- src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
+++ src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
@@ -10,17 +10,6 @@ import java.io.PrintWriter;
 
 public class TOOPInterfaceDC implements ITOOPInterfaceDC {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDC (doGet)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
-    }
-
-    @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String pathInfo = req.getPathInfo();
         final PrintWriter aPW = resp.getWriter();
--- src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDP.java
+++ src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDP.java
@@ -10,17 +10,6 @@ import java.io.PrintWriter;
 
 public class TOOPInterfaceDP implements ITOOPInterfaceDP {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
-        final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDP (doGet)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
-        aPW.flush();
-    }
-
-    @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String pathInfo = req.getPathInfo();
         final PrintWriter aPW = resp.getWriter();
