3b137a9b8fbdc84e7b71edc9d7e11e8af11e6b59
Anton Wiklund <ixuz07@gmail.com>
2018-02-16 18:45:55 +0100

    Unpacks incoming http requests into a ToopMessageBundle and populates the fields with attributes from the bundle.

--- src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
+++ src/main/java/eu/toop/demoui/endpoints/TOOPInterfaceDC.java
@@ -1,19 +1,19 @@
 package eu.toop.demoui.endpoints;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import eu.toop.demoui.bean.Organization;
 import eu.toop.demoui.view.StartView;
 import eu.toop.iface.ITOOPInterfaceDC;
 import eu.toop.iface.mockup.TOOPMessageBundle;
+import eu.toop.iface.mockup.TOOPMessageBundleBuilder;
 
 public class TOOPInterfaceDC implements ITOOPInterfaceDC {
 
@@ -26,25 +26,24 @@ public class TOOPInterfaceDC implements ITOOPInterfaceDC {
     @Override
     public void doPost(final HttpServletRequest req, final HttpServletResponse resp)
 	    throws ServletException, IOException {
-        final String pathInfo = req.getPathInfo();
         final PrintWriter aPW = resp.getWriter();
-        aPW.write("<html><body><h1>TOOPInterfaceDC (doPost)</h1>");
-        aPW.write("<div>Path info: " + pathInfo + "</div>");
-        aPW.write("<div>Parameter map: " + req.getParameterMap() + "</div>");
-        aPW.write("</body></html>");
+        aPW.write("<html><body>OK</body></html>");
         aPW.flush();
 
-        // TODO: Read the HttpServletRequest input stream and deserialize into a TOOPMessageBundle so that we can use its content to populate the organization form in the UI.
+        try {
+            TOOPMessageBundle bundleRead = new TOOPMessageBundleBuilder().parse(req.getInputStream());
+            ui.access(() -> {
+                // Push a new organization bean to the UI
+                if (ui.getNavigator().getCurrentView() instanceof StartView) {
+                    StartView startView = (StartView)ui.getNavigator().getCurrentView();
+                    Organization organization = new Organization();
+                    organization.setCompanyName(bundleRead.getMsDataRequest().identifier); // TODO: Actual data from the retrieved ToopMessageBundle, however it is the wrong attribute. Should be the company name.
+                    organization.setCompanyType("Limited partnership"); // TODO: Replace this mockup data with real data fetched from a TOOPMessageBundle
+                    startView.organizationForm.setOrganizationBean(organization);
+                }
+            });
+        } catch (Exception e) {
 
-        ui.access(() -> {
-            // Push a new organization bean to the UI
-            if (ui.getNavigator().getCurrentView() instanceof StartView) {
-                StartView startView = (StartView)ui.getNavigator().getCurrentView();
-                Organization organization = new Organization();
-                organization.setCompanyName("Zizi mat Ltd"); // TODO: Replace this mockup data with real data fetched from a TOOPMessageBundle
-                organization.setCompanyType("Limited partnership"); // TODO: Replace this mockup data with real data fetched from a TOOPMessageBundle
-                startView.organizationForm.setOrganizationBean(organization);
-            }
-        });
+        }
     }
 }
