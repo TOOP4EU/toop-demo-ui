5d20abdc2c0965d99b1d292bc3c711c40ac85351
Philip Helger <philip@helger.com>
2018-02-27 13:41:35 +0100

    Simplified call

--- src/main/java/eu/toop/demoui/pages/MainCompanyPage.java
+++ src/main/java/eu/toop/demoui/pages/MainCompanyPage.java
@@ -1,50 +1,43 @@
 package eu.toop.demoui.pages;
 
-import com.helger.asic.SignatureHelper;
-import com.helger.commons.collection.impl.CommonsArrayList;
-import com.helger.commons.io.file.FileHelper;
-import com.helger.commons.io.stream.NonBlockingByteArrayOutputStream;
-import com.vaadin.ui.*;
-import eu.toop.commons.doctype.EToopDocumentType;
-import eu.toop.commons.doctype.EToopProcess;
-import eu.toop.commons.exchange.RequestValue;
-import eu.toop.commons.exchange.message.ToopMessageBuilder;
-import eu.toop.commons.exchange.mock.MSDataRequest;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jsoup.UncheckedIOException;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
 import eu.toop.demoui.components.Body;
 import eu.toop.demoui.components.FreedoniaHeader;
 import eu.toop.demoui.form.IdentityForm;
 import eu.toop.demoui.form.MainCompanyForm;
 import eu.toop.demoui.view.StartView;
 import eu.toop.iface.ToopInterfaceManager;
-import eu.toop.iface.mockup.client.HttpClientInvoker;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainCompanyPage extends BasePage {
 
-  private MainCompanyForm mainCompanyForm;
+  private final MainCompanyForm mainCompanyForm;
 
-  public MainCompanyPage (StartView view) {
+  public MainCompanyPage (final StartView view) {
     super (view);
 
     setStyleName ("pageMainCompany");
 
-    VerticalLayout main = new VerticalLayout ();
+    final VerticalLayout main = new VerticalLayout ();
     setHeader (new FreedoniaHeader ());
     setBody (new Body (main));
 
     main.setSizeUndefined ();
 
     {
-      Label label = new Label ("Register a new branch in Freedonia");
+      final Label label = new Label ("Register a new branch in Freedonia");
       main.addComponent (label);
     }
 
     {
-      Label label = new Label ("Your personal details");
+      final Label label = new Label ("Your personal details");
       main.addComponent (label);
     }
 
@@ -52,28 +45,27 @@ public class MainCompanyPage extends BasePage {
     }));
 
     {
-      Label label = new Label ("Parent company main details");
+      final Label label = new Label ("Parent company main details");
       main.addComponent (label);
     }
 
-    Button toopButton = new Button ("Click to pre-fill with TOOP.", clickEvent -> {
-      List<String> conceptList = new ArrayList<> ();
-      conceptList.add ("companyName");
-      conceptList.add ("companyType");
-      ToopInterfaceManager.requestConcepts (conceptList);
+    final Button toopButton = new Button ("Click to pre-fill with TOOP.", clickEvent -> {
+      try {
+        ToopInterfaceManager.requestConcepts (Arrays.asList ("companyName", "companyType"));
+      } catch (final IOException ex) {
+        throw new UncheckedIOException (ex);
+      }
     });
     main.addComponent (toopButton);
 
     mainCompanyForm = new MainCompanyForm (view.getMainCompany (), false, null);
     main.addComponent (mainCompanyForm);
 
-    Button nextButton = new Button ("Confirm and proceed");
+    final Button nextButton = new Button ("Confirm and proceed");
     main.addComponent (nextButton);
-    nextButton.addClickListener (new Button.ClickListener () {
-      public void buttonClick (Button.ClickEvent event) {
-        mainCompanyForm.save ();
-        view.setCurrentPage (new NewCompanyPage (getView ()));
-      }
+    nextButton.addClickListener (event -> {
+      mainCompanyForm.save ();
+      view.setCurrentPage (new NewCompanyPage (getView ()));
     });
   }
 }
