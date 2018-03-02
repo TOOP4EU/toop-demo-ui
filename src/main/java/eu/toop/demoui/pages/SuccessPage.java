f28f47a62b03c54a2e78b8fd3cc6d78ecd7bb08a
Anton Wiklund <ixuz07@gmail.com>
2018-03-02 10:06:08 +0100

    Adjusted the final page

--- src/main/java/eu/toop/demoui/pages/SuccessPage.java
+++ src/main/java/eu/toop/demoui/pages/SuccessPage.java
@@ -1,9 +1,6 @@
 package eu.toop.demoui.pages;
 
-import com.vaadin.ui.Button;
-import com.vaadin.ui.CustomLayout;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.*;
 import eu.toop.demoui.view.HomeView;
 
 public class SuccessPage extends CustomLayout {
@@ -11,5 +8,11 @@ public class SuccessPage extends CustomLayout {
     super ("SuccessPage");
 
     setHeight ("100%");
+
+    String companyName = view.getMainCompany ().getCompanyName ();
+    if (companyName.isEmpty ()) {
+      companyName = "Zizi mat";
+    }
+    addComponent (new Label (companyName), "companyName");
   }
 }
