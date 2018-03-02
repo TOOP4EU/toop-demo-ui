a0cedc3452525a12dace37b935e28cfd6db33c2c
Anton Wiklund <ixuz07@gmail.com>
2018-03-02 10:20:56 +0100

    Added a combobox for country selection

--- src/main/java/eu/toop/demoui/pages/ChooseCountryPage.java
+++ src/main/java/eu/toop/demoui/pages/ChooseCountryPage.java
@@ -1,15 +1,25 @@
 package eu.toop.demoui.pages;
 
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.themes.ValoTheme;
 import eu.toop.demoui.view.HomeView;
+
+import java.util.*;
 
 public class ChooseCountryPage extends CustomLayout {
   public ChooseCountryPage (HomeView view) {
     super ("ChooseCountryPage");
 
     setHeight ("100%");
+
+    final Map<String, String> map = new HashMap<> ();
+    map.put("Elonia", "EL");
+    final ComboBox country = new ComboBox<>("", map.keySet());
+    country.setWidth ("100%");
+    country.setPlaceholder ("Select eID issuer country");
+    addComponent(country, "countryDropdown");
 
     Button nextButton = new Button ("Next");
     nextButton.addStyleName (ValoTheme.BUTTON_BORDERLESS);
