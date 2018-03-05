15fbc2fe2f37b8cd867c15afd9b8db8bda3ef941
Philip Helger <philip@helger.com>
2018-03-05 16:10:01 +0100

    Added missing type

--- src/main/java/eu/toop/demoui/pages/ChooseCountryPage.java
+++ src/main/java/eu/toop/demoui/pages/ChooseCountryPage.java
@@ -1,34 +1,32 @@
 package eu.toop.demoui.pages;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.themes.ValoTheme;
+
 import eu.toop.demoui.view.HomeView;
 
-import java.util.*;
-
 public class ChooseCountryPage extends CustomLayout {
-  public ChooseCountryPage (HomeView view) {
+  public ChooseCountryPage (final HomeView view) {
     super ("ChooseCountryPage");
 
     setHeight ("100%");
 
     final Map<String, String> map = new HashMap<> ();
-    map.put("Elonia", "EL");
-    final ComboBox country = new ComboBox<>("", map.keySet());
+    map.put ("Elonia", "EL");
+    final ComboBox<String> country = new ComboBox<> ("", map.keySet ());
     country.setWidth ("100%");
     country.setPlaceholder ("Select eID issuer country");
-    addComponent(country, "countryDropdown");
+    addComponent (country, "countryDropdown");
 
-    Button nextButton = new Button ("Next");
+    final Button nextButton = new Button ("Next");
     nextButton.addStyleName (ValoTheme.BUTTON_BORDERLESS);
     nextButton.addStyleName (" freedonia");
     addComponent (nextButton, "nextButton");
-    nextButton.addClickListener(new Button.ClickListener() {
-      public void buttonClick(Button.ClickEvent event) {
-        view.setCurrentPage (new IdentityProviderPage (view));
-      }
-    });
+    nextButton.addClickListener (event -> view.setCurrentPage (new IdentityProviderPage (view)));
   }
 }
