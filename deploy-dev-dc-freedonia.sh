64f559310ca582ad43ce4d3a1a67d6917ca96d70
Konstantinos Douloudis <kdoul@unipi.gr>
2019-10-01 13:09:18 +0300

    Update deployment scripts to fully automate deployment

--- deploy-dev-dc-freedonia.sh
+++ deploy-dev-dc-freedonia.sh
@@ -1,23 +1,36 @@
 #/bin/sh
 
-DATE=`date +%Y%m%d`
+TIMESTAMP=`date +%Y%m%d.%H%M%S`
 BASEDIR=/opt/tomcat/webapps
 FILENAME=toop-demo-ui-dc-0.10.5-SNAPSHOT.war
+APP=$(pwd)/toop-demo-ui-dc/target/$FILENAME
+BACKUPDIR=/toop-dir/backups
 
-if [ -f ~/$FILENAME ]
+if ! [ -x "$(command -v mvn)" ]; then
+  echo 'Error: Maven is not installed.' >&2
+  exit 1
+fi
+
+echo Making App
+cd toop-demo-ui-dc
+mvn clean package
+cd ..
+
+if [ -f $APP ]
   then
     echo stopping Tomcat
     sudo service tomcat stop
 
     echo Cleaning exisiting dirs
     cd $BASEDIR
-    sudo mv ROOT/ ~/dc.$DATE
-    sudo chmod 777 ~/dc.$DATE
+    [ ! -d "$BACKUPDIR" ] && mkdir -p "$BACKUPDIR"
+    sudo mv ROOT/ $BACKUPDIR/dc.$TIMESTAMP
+    sudo chmod 777 $BACKUPDIR/dc.$TIMESTAMP
 
     echo ROOT
     APPDIR=$BASEDIR/ROOT
     [ ! -d $APPDIR ] && sudo mkdir $APPDIR
-    sudo cp ~/$FILENAME $APPDIR
+    sudo cp $APP $APPDIR
     cd $APPDIR
     sudo unzip -q $FILENAME && sudo rm $FILENAME
     sudo rm $APPDIR/WEB-INF/classes/private-*.properties
@@ -26,7 +39,9 @@ if [ -f ~/$FILENAME ]
     sudo rm $APPDIR/WEB-INF/classes/toop-interface.properties
     sudo mv $APPDIR/WEB-INF/classes/toop-interface.freedonia-dev.properties $APPDIR/WEB-INF/classes/toop-interface.properties
 
+    echo starting Tomcat 
+    sudo service tomcat start
     echo Done!
   else
-    echo "Source file ~/$FILENAME not existing!"
+    echo "Source file $APP not existing!"
 fi
