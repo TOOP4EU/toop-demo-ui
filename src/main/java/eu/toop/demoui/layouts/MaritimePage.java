29616bf504e932739cdcf7b7c85a650c55fd4866
Maria Siapera <mariaspr@unipi.gr>
2019-10-17 15:02:10 +0300

    NMA request: Moved download certificate button to the left. Stretch out window so that there is not scrolling

--- src/main/java/eu/toop/demoui/layouts/MaritimePage.java
+++ src/main/java/eu/toop/demoui/layouts/MaritimePage.java
@@ -408,18 +408,18 @@ public class MaritimePage extends CustomLayout {
 
         Grid<DocumentDataBean> grid = new Grid<>();
         grid.setItems(docResponseList);
-        grid.addColumn(DocumentDataBean::getDocumentName).setCaption("Document Name");
-        grid.addColumn(DocumentDataBean::getDocumentDescription).setCaption("Document Description");
-        grid.addColumn(DocumentDataBean::getDocumentURI).setCaption("URI");
-        grid.addColumn(DocumentDataBean::getDocumentIdentifier).setCaption("Document Identifier");
-        grid.addColumn(DocumentDataBean::getDocumentIssuePlace).setCaption("Issue Place");
-        grid.addColumn(DocumentDataBean::getDocumentIssueDate).setCaption("Issue Date");
-        grid.addColumn(DocumentDataBean::getDocumentMIMEType).setCaption("MIME Type code");
         grid.addComponentColumn(DocumentDataBean -> {
             Button certificateButton = getCertificateButton(DocumentDataBean.getDocumentURI());
             return certificateButton;
         });
-        grid.setWidth("980");
+        grid.addColumn(DocumentDataBean::getDocumentName).setCaption("Document Name");
+        grid.addColumn(DocumentDataBean::getDocumentDescription).setCaption("Document Description");
+        grid.addColumn(DocumentDataBean::getDocumentIssuePlace).setCaption("Issue Place");
+        grid.addColumn(DocumentDataBean::getDocumentIssueDate).setCaption("Issue Date");
+        grid.addColumn(DocumentDataBean::getDocumentURI).setCaption("URI");
+        grid.addColumn(DocumentDataBean::getDocumentIdentifier).setCaption("Document Identifier");
+        grid.addColumn(DocumentDataBean::getDocumentMIMEType).setCaption("MIME Type code");
+        grid.setWidth("1500");
 //        return grid;
         addComponent(grid, "documentCertificateList");
 
