632292569774b01261dec98500ad4fad4f163ea6
Philip Helger <philip@helger.com>
2020-01-08 15:51:01 +0100

    Updated to 2020

--- toop-demo-ui-dc-maritime/src/main/java/eu/toop/demoui/layouts/MaritimePage.java
+++ toop-demo-ui-dc-maritime/src/main/java/eu/toop/demoui/layouts/MaritimePage.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018-2019 toop.eu
+ * Copyright (C) 2018-2020 toop.eu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package eu.toop.demoui.layouts;
 
 import java.io.IOException;
